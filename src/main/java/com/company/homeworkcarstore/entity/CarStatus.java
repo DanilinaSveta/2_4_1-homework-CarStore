ba9805e0291612c46a75f7e3bd1f168b15d2bb71
DanilinaSveta <danilina.sveta14@yandex.ru>
2025-01-07 16:47:52 +0300

    my

---
+++ src/main/java/com/company/homeworkcarstore/entity/CarStatus.java
@@ -0,0 +1,31 @@
+package com.company.homeworkcarstore.entity;
+
+import io.jmix.core.metamodel.datatype.EnumClass;
+
+import org.springframework.lang.Nullable;
+
+
+public enum CarStatus implements EnumClass<String> {
+
+    ;
+
+    private final String id;
+
+    CarStatus(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Nullable
+    public static CarStatus fromId(String id) {
+        for (CarStatus at : CarStatus.values()) {
+            if (at.getId().equals(id)) {
+                return at;
+            }
+        }
+        return null;
+    }
+}
